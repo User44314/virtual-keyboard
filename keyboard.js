37e19768beb861ebdc6f6d3e49b344d43336f717
User44314 <user1234aaaa@gmail.com>
2023-04-28 21:29:42 +0300

    feat: classes for body and main atribute creat keys in keyboards.js

--- keyboard.js
+++ keyboard.js
@@ -29,6 +29,39 @@ const keyboard = {
     document.body.appendChild(this.elements.main);
   },
 
+  _createkeys() {
+    const fragment = document.createDocumentFragment();
+    const keylayout = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+      "space"
+    ];
+
+    const createiconshtml = (icon_name) =>{
+      return "<i claas = materials-icons><i>${icon_name}</i>";
+    };
+
+    keylayout.forEach(key => {
+      const keyelement = document.createElement("button");
+      const inserlinebreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+      //add attributes/clasess
+      keyelement.setAttribute("type", "button");
+      keyelement.classList.add("keyboard-key");
+      
+
+      switch(key){
+        case "backspace":
+          keyelement.classList.add("keyboard-key-wide");
+          keyelement.innerHTML = createiconshtml("backspace");
+
+          break;
+      }
+    });
+
+  },
+
   _triggerEvent(handlerName) {
     console.log("event triggered event name:" + handlerName);
   },
